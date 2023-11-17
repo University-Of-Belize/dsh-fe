29b70cc4a9e2daebc60503cbad444933e5a0a111
AlexDev404 <alexdev404@users.noreply.github.com>
2023-11-17 00:40:28 -0600

    Hate this way of importing files. But it won't build unless I do

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,11 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import pkg from './package.json' assert { type: 'json' };
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);  // Hate this way of importing files. But it won't build unless I do
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
