0fd9da21b39f1b7e6b354d7c21016a0164ada308
AlexDev404 <AlexDev404@users.noreply.github.com>
2023-12-23 15:38:45 -0600

    Improvements to sitemap generation

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -2,50 +2,47 @@
 // Thanks, Alex!
 
 import dirTree from 'directory-tree';
-import * as fs from "fs";
-import config from "../src/lib/config/sitemap-settings"
+import * as fs from 'fs';
+import config from '../src/lib/config/sitemap-settings';
+import xmlFormat from 'xml-formatter';
 
-const baseRoute = "/";
-const routes: string[] = [baseRoute]
-const date = new Date().toISOString().split('T')[0]
+const baseRoute = '/';
+const routes: string[] = [baseRoute];
+const date = new Date().toISOString().split('T')[0];
 
 function getSitemapXML(domain: string, routes: string[]) {
-    let sitemap = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-    sitemap += "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n"
-    routes.forEach(route => {
-        sitemap += getSitemapUrl(domain + route)
-    })
-    sitemap += "\n</urlset>"
-    return sitemap;
+	let sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n';
+	sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+	routes.forEach((route) => {
+		sitemap += getSitemapUrl(domain + route);
+	});
+	sitemap += '\n</urlset>';
+	return sitemap;
 }
 
 function getSitemapUrl(location: string) {
-    const url =
-        "<url>\n" +
-        `<loc>${location}</loc>\n` +
-        `<lastmod>${date}</lastmod>\n` +
-        "</url>";
-    return url
+	const url = '<url>\n' + `<loc>${location}</loc>\n` + `<lastmod>${date}</lastmod>\n` + '</url>';
+	return url;
 }
 
 function getEndpoints(tree: dirTree.DirectoryTree, route: string) {
-    tree.children!.forEach(child => {
-        if (child.children != undefined && child.children.length != 0) {
-            let childRoute = route + child.name;
-            if (child.children.some(e => e.name === '+page.svelte')) {
-                routes.push(childRoute)
-            }
-            getEndpoints(child, childRoute + "/");
-        }
-    })
+	tree.children!.forEach((child) => {
+		if (child.children != undefined && child.children.length != 0) {
+			let childRoute = route + child.name;
+			if (child.children.some((e) => e.name === '+page.svelte')) {
+				routes.push(childRoute);
+			}
+			getEndpoints(child, childRoute + '/');
+		}
+	});
 }
 
-const tree = dirTree("./src/routes")
+const tree = dirTree('./src/routes');
 
 getEndpoints(tree, baseRoute);
 
 // YOUR_DOMAIN should be like https://example.com
-const sitemap = getSitemapXML(config.server['bound-domain'], routes)
+const sitemap = getSitemapXML(config.server['bound-domain'], routes);
 
 // If you use the script in postbuild mode use
 // For vercel deployment use:
@@ -53,4 +50,7 @@ const sitemap = getSitemapXML(config.server['bound-domain'], routes)
 // fs.writeFileSync('.svelte-kit/output/client/sitemap.xml', sitemap);
 
 // If you use the script in prebuild mode use
-fs.writeFileSync('static/sitemap.xml', sitemap);
+fs.writeFileSync(
+	'static/sitemap.xml',
+	xmlFormat(sitemap, { indentation: '   ', lineSeparator: '\n', collapseContent: true })
+);
