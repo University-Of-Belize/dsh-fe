4befed40b35a72f2bd6e5f8070f6cc680d790ae2
AlexDev404 <AlexDev404@users.noreply.github.com>
2024-01-15 12:55:53 -0600

    Switch server

--- src/lib/config/settings.ts
+++ src/lib/config/settings.ts
@@ -2,8 +2,8 @@ import { version } from '$app/environment';
 
 const settings = {
 	server: {
-		// HTTPOrigin: 'https://winter-darkness-1705.fly.dev',
-		HTTPOrigin: 'http://127.0.0.1:8080',
+		HTTPOrigin: 'https://winter-darkness-1705.fly.dev',
+		// HTTPOrigin: 'http://127.0.0.1:8080',
 		// HTTPOrigin: 'http://192.168.1.102:8080',
 		'support-email': 'cafeteria@ub.edu.bz',
 		'bound-organization': 'University of Belize',
