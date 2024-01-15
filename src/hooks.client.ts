870d4a28ae18a15cfa53888b9936aebf37ff675e
AlexDev404 <AlexDev404@users.noreply.github.com>
2024-01-15 00:13:07 -0600

    Setup Sentry. Works well! :)

---
+++ src/hooks.client.ts
@@ -0,0 +1,23 @@
+import { handleErrorWithSentry, Replay } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: 'https://537f9ebddaa9b420f4927ab5304c417a@o4506573592723456.ingest.sentry.io/4506573594755072',
+	tracesSampleRate: 1.0,
+
+	// This enables Session Replay, but you need to replace the URL with the
+	// URL provided to you in the Sentry UI.
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
+
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
+
+	// If you don't want to use Session Replay, just remove the line below:
+	integrations: [new Replay()]
+});
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
---
+++ src/hooks.server.ts
@@ -0,0 +1,14 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: 'https://537f9ebddaa9b420f4927ab5304c417a@o4506573592723456.ingest.sentry.io/4506573594755072',
+  tracesSampleRate: 1.0,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
