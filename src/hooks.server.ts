faafc54e3fd7794e82754634e99f68ee5a5a7486
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2024-07-24 11:28:22 -0600

    Manually set release in `hooks.server.ts`

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,15 @@ import { sequence } from '@sveltejs/kit/hooks';
 import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json); // Hate this way of importing files. But it won't build unless I do
+
 Sentry.init({
+	release: `${pkg.name}@${pkg.version}`
 	dsn: 'https://537f9ebddaa9b420f4927ab5304c417a@o4506573592723456.ingest.sentry.io/4506573594755072',
 	tracesSampleRate: 1.0
 });
