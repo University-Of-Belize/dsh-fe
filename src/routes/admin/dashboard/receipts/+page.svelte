67c2e0f0e21f2e70caec4fe91e9e5e5aa4d92b5e
AlexDev404 <alexdev404@users.noreply.github.com>
2024-07-28 02:16:36 -0600

    Finish Excel enhanced export capabilities

--- src/routes/admin/dashboard/receipts/+page.svelte
+++ src/routes/admin/dashboard/receipts/+page.svelte
@@ -5,17 +5,16 @@
 	import type { Order } from '$lib/types/Order';
 	import type { Promo } from '$lib/types/Promo';
 	import type { User } from '$lib/types/User';
-	import { what_is } from '$lib/vendor/dishout/What_Is';
-	import { faFileCsv } from '@fortawesome/free-solid-svg-icons';
+	import { faFileCsv, faFileExcel } from '@fortawesome/free-solid-svg-icons';
 // What is what?
 	import { locateNodeUsingHash } from '$lib/Elements/Utility/page';
 	import type { CartProduct } from '$lib/types/Product';
-	import what from '$lib/vendor/dishout/Whats';
 	import { fetchWebApi } from '$lib/vendor/dishout/api';
 	import { toast } from '@zerodevx/svelte-toast';
-	import { saveAs } from 'file-saver';
+	import saveAs from 'file-saver';
 	import * as converter from 'json-2-csv';
 	import _ from 'lodash';
+	import * as xlsx from 'xlsx';
 
 	import { getLocaleDateTime } from '$lib/Elements/Utility/time';
 	import { onMount } from 'svelte';
@@ -98,163 +97,6 @@
 	});
 
 	const getId = (id: string) => document.getElementById(id);
-	function go_order(action: number, orderId: string) {
-		switch (action) {
-			case 1: // Accept
-				toast.push('You accepted this order.');
-				getId(`order-${orderId}`)?.classList.remove('border-red-300'); // @ts-ignore
-				getId(`order-${orderId}`)?.classList.remove('border-COLORACCENTL'); // @ts-ignore
-				if (drawerOpenBy != 1) {
-					// Keepthe drawer open as long as we're switching tabs
-					getId(`pulldown-${orderId}`)?.classList.add('hidden'); // @ts-ignore
-					drawerOpenBy = 1;
-				}
-				getId(`pulldown-${orderId}`)?.classList.toggle('hidden'); // @ts-ignore
-				getId(`pulldown-content-${orderId}`)?.classList.remove('hidden'); // @ts-ignore
-				getId(`modify-content-${orderId}`)?.classList.add('hidden'); // @ts-ignore
-				getId(`user-modify-content-${orderId}`)?.classList.add('hidden');
-				getId(`title-${orderId}`).innerHTML =
-					'You <b class="font-normal text-COLORLIGHT-100">accepted</b> this order.';
-				currentAction = [action, orderId];
-				break;
-			case 2: // Decline
-				toast.push(staff ? 'You rejected this order.' : "You're about to delete this order.");
-				getId(`order-${orderId}`)?.classList.remove('border-COLORACCENTL'); // @ts-ignore
-				getId(`order-${orderId}`)?.classList.add('border-red-300'); // @ts-ignore
-				if (drawerOpenBy != 2) {
-					// Keep the drawer open as long as we're switching tabs
-					getId(`pulldown-${orderId}`)?.classList.add('hidden'); // @ts-ignore
-					drawerOpenBy = 2;
-				}
-				getId(`pulldown-${orderId}`)?.classList.toggle('hidden'); // @ts-ignore
-				getId(`pulldown-content-${orderId}`)?.classList.add('hidden'); // @ts-ignore
-				getId(`modify-content-${orderId}`)?.classList.add('hidden'); // @ts-ignore
-				getId(`user-modify-content-${orderId}`)?.classList.add('hidden');
-				getId(`title-${orderId}`).innerHTML = staff
-					? 'You <b class="font-normal text-COLORACCENTL">rejected</b> this order.'
-					: 'Are you <b class="font-normal text-COLORACCENTL underline">absolutely sure</b> you want to remove this order?';
-				currentAction = [action, orderId];
-				break;
-			case 3: // Modify
-				toast.push("You're altering this order.");
-				// @ts-ignore
-				getId(`title-${orderId}`).innerHTML =
-					'You\'re <b class="font-normal text-COLORACCENTL">altering</b> this order.';
-				getId(`order-${orderId}`)?.classList.remove('border-red-300'); // @ts-ignore
-				getId(`order-${orderId}`)?.classList.add('border-COLORACCENTL'); // @ts-ignore
-				if (drawerOpenBy != 3) {
-					// Keep the drawer open as long as we're switching tabs
-					getId(`pulldown-${orderId}`)?.classList.add('hidden'); // @ts-ignore
-					drawerOpenBy = 3;
-				}
-				getId(`pulldown-${orderId}`)?.classList.toggle('hidden'); // @ts-ignore
-				if (staff) {
-					getId(`pulldown-content-${orderId}`)?.classList.remove('hidden'); // @ts-ignore
-					getId(`modify-content-${orderId}`)?.classList.remove('hidden');
-					getId(`user-modify-content-${orderId}`)?.classList.remove('hidden');
-				} else {
-					getId(`pulldown-content-${orderId}`)?.classList.add('hidden'); // @ts-ignore
-					getId(`modify-content-${orderId}`)?.classList.add('hidden');
-					getId(`user-modify-content-${orderId}`)?.classList.remove('hidden');
-				}
-				currentAction = [action, orderId];
-				break;
-			case 4: // Ready
-				toast.push("You're readying this order.");
-				getId(`order-${orderId}`)?.classList.remove('border-COLORACCENTL'); // @ts-ignore
-				getId(`order-${orderId}`)?.classList.add('border-red-300'); // @ts-ignore
-				if (drawerOpenBy != 4) {
-					// Keep the drawer open as long as we're switching tabs
-					getId(`pulldown-${orderId}`)?.classList.add('hidden'); // @ts-ignore
-					drawerOpenBy = 4;
-				}
-				getId(`pulldown-${orderId}`)?.classList.toggle('hidden'); // @ts-ignore
-				getId(`pulldown-content-${orderId}`)?.classList.add('hidden'); // @ts-ignore
-				getId(`modify-content-${orderId}`)?.classList.add('hidden'); // @ts-ignore
-				getId(`user-modify-content-${orderId}`)?.classList.add('hidden');
-				getId(`title-${orderId}`).innerHTML =
-					'This will send out the "ready" status to the user.<br/>Are you <b class="font-normal text-COLORACCENTL underline">absolutely sure</b> this order is ready for pick-up?';
-				currentAction = [action, orderId];
-				break;
-			default: // WTF
-				toast.push('The parameters provided are incorrect.');
-		}
-	}
-
-	async function processRequest(event: Event) {
-		event.preventDefault();
-		console.log('Processing request...');
-		// @ts-ignore
-		const valueArray = Array.from(event.target)
-			.filter((el) => el.name)
-			.map((el) => el.value);
-
-		// If we're not deleting, or readying the order...
-		// convert the delivery time back to a UNIX_TS
-		if (currentAction[0] !== 2 && currentAction[0] !== 4) {
-			// Convert back to timestamps because, we need those.
-			valueArray[valueArray.length - 3] = Date.parse(valueArray[valueArray.length - 3]) / 1000;
-		} else {
-			valueArray[1] = null; // JSON parseable value
-		}
-		console.log(valueArray);
-		switch (
-			currentAction[0] // The action
-		) {
-			case 1: // Accept
-				// Hide this so that we can't use the accept button anymore
-				getId(`pulldown-${currentAction[1]}`)?.classList.add('hidden');
-			case 2: // Decline
-				// Hide this because it's bugged
-				getId(`pulldown-${currentAction[1]}`)?.classList.add('hidden');
-			case 3: // Modify
-			case 4: // Ready
-				const r = (await fetchWebApi(
-					'admin/order/manage',
-					'POST',
-					what_is(what.private.order, [
-						currentAction[0] === 1
-							? 'a'
-							: currentAction[0] === 2
-								? 'd'
-								: currentAction[0] === 3
-									? 'm'
-									: currentAction[0] === 4
-										? 'r'
-										: '?', // Ternary statement to map 'a' for ACCEPT /
-						// 'd' for DELETE / 'm' for MODIFY / 'r' for READY
-						currentAction[1], // Order ID
-						JSON.parse(valueArray[1]), // The price (We use JSON.parse to turn the string back into a 'real' number/float)
-						valueArray[2] === 'placeholder' ? null : valueArray[2], // The promotion
-						valueArray[0] // The ETA
-					]) // Lol
-				)) as Response;
-				if (!r.ok) {
-					try {
-						const res = await r.json();
-						toast.push(res.message);
-					} catch (error) {
-						toast.push(`Failed: ${error}`);
-					}
-					return;
-				}
-				const res = await r.json();
-				toast.push(
-					currentAction[0] === 1
-						? 'Order accepted.'
-						: currentAction[0] === 2
-							? res.is
-							: currentAction[0] === 3
-								? 'Update successfully issued.'
-								: 'Update successfully issued.'
-				);
-				catchAll(); // Issue UI update
-				break;
-
-			default: // What?
-				break;
-		}
-	}
 </script>
 
 <svelte:head>
@@ -266,69 +108,182 @@
 <div
 	class="content block h-full w-full overflow-auto bg-transparent p-2 py-8 pb-40 lg:px-16 lg:py-16"
 >
-	<div class="flex-header lg:flex block w-full flex-wrap items-start">
+	<div class="flex-header block w-full flex-wrap items-start lg:flex">
 		<div class="block">
 			<div class="flex pb-2 text-2xl font-semibold">View receipts</div>
-			<div class="flex pb-4 lg:pb-12 text-xl font-light">View closed orders</div>
+			<div class="flex pb-4 text-xl font-light lg:pb-12">View closed orders</div>
 		</div>
-		<div class="flex flex-1 justify-start lg:justify-end pb-2 text-2xl font-semibold">
+		<div
+			class="block flex-1 justify-start space-y-2 pb-2 text-2xl font-semibold lg:flex lg:justify-end lg:space-x-2 lg:space-y-0"
+		>
 			<Button
 				text="Download Spreadsheet"
 				icon={faFileCsv}
 				on:click={async () => {
-                    let data_ = JSON.parse(JSON.stringify(data));
+					let data_ = JSON.parse(JSON.stringify(data));
 
-                    // Remove unnecessary data
-                    data_.forEach((order) => {
-                        delete order._id;
+					// Remove unnecessary data
+					data_.forEach((order) => {
+						delete order._id;
 
+						order['Order Code'] = order.order_code;
+						delete order.order_code;
 
-                        order["Order Code"] = order.order_code;
-                        delete order.order_code;
+						order['Is Accepted'] = order.is_accepted ? 'Yes' : 'No';
+						delete order.is_accepted;
 
-                        order["Is Accepted"] = order.is_accepted? "Yes": "No";
-                        delete order.is_accepted;
+						order['Is Completed'] = order.completed ? 'Yes' : 'No';
+						delete order.completed;
 
-                        order["Is Completed"] = order.completed? "Yes": "No";
-                        delete order.completed;
+						order['Order From (Username)'] = order.order_from.username;
+						order['Order From (Email)'] = order.order_from.email;
+						delete order.order_from;
 
-                        order["Order From (Username)"] = order.order_from.username;
-                        order["Order From (Email)"] = order.order_from.email;
-                        delete order.order_from;
-                        
-                        // order["Accepted by (Username)"] = order?.override_by.username ?? "N/A";
-                        // order["Accepted by (Email)"] = order?.override_by.email ?? "N/A";
-                        delete order.override_by;
+						// order["Accepted by (Username)"] = order?.override_by.username ?? "N/A";
+						// order["Accepted by (Email)"] = order?.override_by.email ?? "N/A";
+						delete order.override_by;
 
-                        order["Order Date"] = getLocaleDateTime(order.order_date)
-                        delete order.order_date;
+						order['Order Date'] = getLocaleDateTime(order.order_date);
+						delete order.order_date;
 
-                        order["Products"] = "Various";
-                        delete order.products;
+						order['Products'] = 'Various';
+						// delete order.products;
 
-                        // order["Delay Time (seconds)"] = parseFloat(order.delay_time) - parseFloat(order.order_date) + " seconds";
-                        delete order.delay_time;
+						// order["Delay Time (seconds)"] = parseFloat(order.delay_time) - parseFloat(order.order_date) + " seconds";
+						delete order.delay_time;
 
-                        // order["Promo Code"] = order.promo_code.code;
-                        // order["Promo Discount (%)"] = order.promo_code.discount_percentage;
-                        delete order.promo_code;
+						// order["Promo Code"] = order.promo_code.code;
+						// order["Promo Discount (%)"] = order.promo_code.discount_percentage;
+						delete order.promo_code;
 
-                        order["Total Amount ($)"] = order.total_amount.$numberDecimal;
-                        delete order.total_amount;
+						order['Total Amount ($)'] = order.total_amount.$numberDecimal;
+						delete order.total_amount;
 
-                        order["Discount Amount ($)"] = order.discount_amount.$numberDecimal;
-                        delete order.discount_amount;
+						order['Discount Amount ($)'] = order.discount_amount.$numberDecimal;
+						delete order.discount_amount;
 
-                        order["Final amount ($)"] = order.final_amount.$numberDecimal;
-                        delete order.final_amount;
+						order['Final amount ($)'] = order.final_amount.$numberDecimal;
+						delete order.final_amount;
 
-                        delete order.__v;
-                    })
+						delete order.__v;
+					});
 
 					const csv = await converter.json2csv(data_);
 					// Create the blob
 					const blob = new Blob([csv], { type: 'text/csv;charset=utf-8' });
 					saveAs(blob, 'closed_orders.csv');
+				}}
+			/>
+			<Button
+				text="Download XLSX"
+				icon={faFileExcel}
+				on:click={async () => {
+					let data_ = JSON.parse(JSON.stringify(data));
+					const workbook = xlsx.utils.book_new();
+
+					// Remove unnecessary data
+					data_.forEach((order) => {
+						delete order._id;
+
+						order['Order Code'] = order.order_code;
+						delete order.order_code;
+
+						order['Is Accepted'] = order.is_accepted ? 'Yes' : 'No';
+						delete order.is_accepted;
+
+						order['Is Completed'] = order.completed ? 'Yes' : 'No';
+						delete order.completed;
+
+						order['Order From (Username)'] = order.order_from.username;
+						order['Order From (Email)'] = order.order_from.email;
+						delete order.order_from;
+
+						// order["Accepted by (Username)"] = order?.override_by.username ?? "N/A";
+						// order["Accepted by (Email)"] = order?.override_by.email ?? "N/A";
+						delete order.override_by;
+
+						order['Order Date'] = getLocaleDateTime(order.order_date);
+						delete order.order_date;
+
+						// order["Products"] = "Various";
+						// Check if "Products Sold" sheet exists
+						// Format order.products accordingly
+						order.products.forEach((product) => {
+							product['Order Code'] = order['Order Code'];
+
+							product['Product Name'] = product.product.productName;
+							delete product.product.productName;
+
+							product['Product Price ($)'] = JSON.parse(product.product.price.$numberDecimal);
+							delete product.product.price;
+
+							product['Quantity Bought'] = JSON.parse(product.quantity);
+							delete product.quantity;
+
+                            product['Product Price x Quantity Bought'] = product['Product Price ($)'] * product['Quantity Bought'];
+
+							product['Product Slug'] = product.product.slug;
+							delete product.product.slug;
+
+							product['In Stock'] = JSON.parse(product.product.in_stock);
+							delete product.product.in_stock;
+
+							product['Product Description'] = product.product.description;
+							delete product.product.description;
+
+							delete product.product;
+							delete product.variations;
+							delete product._id;
+						});
+
+						if (workbook.SheetNames.includes('Products Sold')) {
+							// Append to the existing sheet
+							const worksheet = workbook.Sheets['Products Sold'];
+							let worksheet_data = xlsx.utils.sheet_to_json(worksheet);
+							worksheet_data = worksheet_data.concat(order.products);
+                            console.log(worksheet_data)
+							// Recreate the sheet with the updated data
+							const updatedWorksheet = xlsx.utils.json_to_sheet(worksheet_data);
+							workbook.Sheets['Products Sold'] = updatedWorksheet;
+						} else {
+							// Create a new sheet
+							const worksheet = xlsx.utils.json_to_sheet(order.products);
+							workbook.SheetNames.push('Products Sold');
+							workbook.Sheets['Products Sold'] = worksheet;
+						}
+
+						delete order.products;
+
+						// delete order.products;
+
+						// order["Delay Time (seconds)"] = parseFloat(order.delay_time) - parseFloat(order.order_date) + " seconds";
+						delete order.delay_time;
+
+						// order["Promo Code"] = order.promo_code.code;
+						// order["Promo Discount (%)"] = order.promo_code.discount_percentage;
+						delete order.promo_code;
+
+						order['Total Amount ($)'] = order.total_amount.$numberDecimal;
+						delete order.total_amount;
+
+						order['Discount Amount ($)'] = order.discount_amount.$numberDecimal;
+						delete order.discount_amount;
+
+						order['Final amount ($)'] = order.final_amount.$numberDecimal;
+						delete order.final_amount;
+
+						delete order.__v;
+					});
+
+					const worksheet = xlsx.utils.json_to_sheet(data_);
+					workbook.SheetNames.push('Closed Orders');
+					workbook.Sheets['Closed Orders'] = worksheet;
+
+					// Reorder the sheets
+					workbook.SheetNames = ['Closed Orders', 'Products Sold'];
+
+					// Save the file
+					xlsx.writeFile(workbook, 'closed_orders.xlsx');
 				}}
 			/>
 		</div>
