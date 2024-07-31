abad156dcd2bfac955370e6a05a166dc6d4e4db0
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2024-07-31 00:35:16 -0600

    Change back origin
    From demo mode

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -2,9 +2,9 @@ import { version } from '$app/environment';
 
 const settings = {
 	server: {
-		// HTTPOrigin: 'https://winter-darkness-1705.fly.dev',
+		HTTPOrigin: 'https://winter-darkness-1705.fly.dev',
 		'api-version': '2',
-		HTTPOrigin: 'https://icy-rain-2400.fly.dev',
+		// HTTPOrigin: 'https://icy-rain-2400.fly.dev',
 		// HTTPOrigin: 'http://127.0.0.1:8080',
 		// HTTPOrigin: 'http://192.168.38.16:8080',
 		// HTTPOrigin: 'http://192.168.1.102:8080',
